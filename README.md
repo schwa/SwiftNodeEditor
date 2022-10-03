08d2d7892e7acd461c45ea6cc85f114dfa7569ee
Jonathan Wight <jwight@mac.com>
2022-10-02 23:25:16 -0700

    Initial swift package.

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftNodeEditor
+
+A description of this package.
---
+++ Sources/SwiftNodeEditor/SwiftNodeEditor.swift
@@ -0,0 +1,6 @@
+public struct SwiftNodeEditor {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
